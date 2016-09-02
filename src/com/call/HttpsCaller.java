9eb4bc6fff8cf0d5b4a79d11ffc29227591a04ea
xujian <xujian_xtkf@win-sky.com.cn>
2016-09-02 20:34:28 +0800

    version 0.1.2

--- src/com/call/HttpsCaller.java
+++ src/com/call/HttpsCaller.java
@@ -1,5 +1,9 @@
 package com.call;
 
+import com.util.CommonDataUtil;
+import com.util.MyHttpRequest;
+import net.sf.json.JSONObject;
+
 /**
  * 调用微信接口，发送https协议的请求
  *
@@ -7,5 +11,41 @@ package com.call;
  * @Create 2016-09-02-10:10
  */
 public class HttpsCaller {
+    /**
+     * 获取微信的Acess token
+     */
+    public String getAcessToken(){
+        String result = "";
+        String expires_in = "";
+        String errcode = "",errmsg = "";
+        String url = "https://api.weixin.qq.com/cgi-bin/token";
+        String params = "grant_type=client_credential&appid="+ CommonDataUtil.APP_ID +"&secret=" + CommonDataUtil.APP_SECRET;
+        String resp = MyHttpRequest.sendGet(url, params);
+        JSONObject jsonObject = JSONObject.fromObject(resp);
+        if(jsonObject.containsKey("access_token")){
+            result = jsonObject.getString("access_token");
+            expires_in = jsonObject.getString("expires_in");
+            System.out.println("凭证有效时间，单位：秒" + expires_in);
+        }else if(jsonObject.containsKey("errcode")){
+            errcode = jsonObject.getString("errcode");
+            errmsg = jsonObject.getString("errmsg");
+            System.out.println("错误码:" + errcode + "错误信息:" + errmsg);
+        }else{
+            result = "请求异常";
+        }
+        return result;
+    }
 
+    /**
+     * 调用统一下单接口，获取预付款，prepay
+     * @param payment
+     * @param ip
+     * @param code
+     * @return
+     */
+    public static String getOrderNum(String payment, String ip, String code){
+        String result = "";
+
+        return result;
+    }
 }
--- src/com/util/CommonDataUtil.java
+++ src/com/util/CommonDataUtil.java
@@ -9,4 +9,6 @@ package com.util;
 public class CommonDataUtil {
     public static final String APP_ID = "wx50ef59c6c983ed4f";
     public static final String APP_SECRET = "2988cd41c5dd5ba3d8eb361b9a321e16";
+    public static final String MCH_ID = "1349387801";
+    public static final String KEY = "e10adc3949ba59abbe56e057f20f0027";
 }
