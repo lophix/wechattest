36d2a983f1199a6c700b5f3c1778e0fc540dfe25
xujian <xujian_xtkf@win-sky.com.cn>
2016-09-02 13:20:22 +0800

    Initial commit

---
+++ src/com/call/HttpsCaller.java
@@ -0,0 +1,11 @@
+package com.call;
+
+/**
+ * 调用微信接口，发送https协议的请求
+ *
+ * @Authuor Administrator
+ * @Create 2016-09-02-10:10
+ */
+public class HttpsCaller {
+
+}
---
+++ src/com/util/MyHttpRequest.java
@@ -0,0 +1,63 @@
+package com.util;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.net.URL;
+import java.net.URLConnection;
+
+/**
+ * 发送URL请求的工具类
+ *
+ * @Authuor Administrator
+ * @Create 2016-09-02-11:19
+ */
+public class MyHttpRequest {
+    public static String sendPost(String url, String params) {
+        String result = "";
+        BufferedReader bufferedReader = null;
+        try {
+            URL realUrl = new URL(url);
+            URLConnection connection = realUrl.openConnection();
+            connection.setRequestProperty("accept", "*/*");
+            connection.setRequestProperty("connection", "Keep-Alive");
+            connection.setRequestProperty("user-agent", "Mozilla/5.0 (Windows NT 6.1; WOW64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/32.0.1700.107 Safari/537.36");
+            connection.setRequestProperty("Charsert", "UTF-8");
+            connection.connect();
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+        return result;
+    }
+
+    public static String sendGet(String url, String params) {
+        String result = "";
+        String sendUrl = url + "?" + params;
+        BufferedReader bufferedReader = null;
+        try {
+            URL realUrl = new URL(sendUrl);
+            URLConnection urlConnection = realUrl.openConnection();
+            urlConnection.setRequestProperty("accept", "*/*");
+            urlConnection.setRequestProperty("connection", "Keep-Alive");
+            urlConnection.setRequestProperty("user-agent", "Mozilla/5.0 (Windows NT 6.1; WOW64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/32.0.1700.107 Safari/537.36");
+            urlConnection.setRequestProperty("Charsert", "UTF-8");
+            urlConnection.connect();
+            bufferedReader = new BufferedReader(new InputStreamReader(urlConnection.getInputStream(), "utf-8"));
+            String resp;
+            while ((resp = bufferedReader.readLine()) != null) {
+                result += resp;
+            }
+        } catch (Exception e) {
+            e.printStackTrace();
+        } finally {
+            try {
+                if (bufferedReader != null) {
+                    bufferedReader.close();
+                }
+            } catch (IOException e) {
+                e.printStackTrace();
+            }
+        }
+        return result;
+    }
+}
